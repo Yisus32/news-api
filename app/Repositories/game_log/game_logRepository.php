4ea68802f9538835e19312bb23ed92707b39f5cd
henry <henryrom57@gmail.com>
2021-07-26 09:32:04 -0400

    cambios fecha

--- app/Repositories/game_log/game_logRepository.php
+++ app/Repositories/game_log/game_logRepository.php
@@ -23,7 +23,7 @@ public function _index($request = null, $user = null)
     $game=DB::table('game_log')
     ->join('group','group.id','=','game_log.gro_id')
     ->join('cars_golf','cars_golf.id','=','game_log.car_id')
-    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name')->get();  
+    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name','game_log.created_at')->get();  
     return $game;
 }
 
@@ -36,7 +36,7 @@ public function _show($id)
     $game=DB::table('game_log')->where('game_log.id',$id)
     ->join('group','group.id','=','game_log.gro_id')
     ->join('cars_golf','cars_golf.id','=','game_log.car_id')
-    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name')->get();  
+    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name','game_log.created_at')->get();  
     return $game;
     }
     
