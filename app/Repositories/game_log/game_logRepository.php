a90f4d1e83d01387b28b6c62ff34d5168c835eff
henry <henryrom57@gmail.com>
2021-07-26 10:38:36 -0400

    holename

--- app/Repositories/game_log/game_logRepository.php
+++ app/Repositories/game_log/game_logRepository.php
@@ -23,7 +23,8 @@ public function _index($request = null, $user = null)
     $game=DB::table('game_log')
     ->join('group','group.id','=','game_log.gro_id')
     ->join('cars_golf','cars_golf.id','=','game_log.car_id')
-    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name','game_log.created_at as fecha')->get();  
+    ->join('holes','holes.id','=','game_log.id_hole')
+    ->select('group.cod as codegroup','game_log.id','game_log.user_id','game_log.auser_id','game_log.car_id','game_log.hol_id','game_log.gro_id','game_log.id_hole','cars_golf.cod as numcar','game_log.user_name','game_log.created_at as fecha','holes.name as namehole')->get();  
     return $game;
 }
 
