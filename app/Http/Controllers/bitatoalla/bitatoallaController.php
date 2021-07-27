7f290ef8ae3b013294b8329aaa0d320421b59e11
henry <henryrom57@gmail.com>
2021-07-27 14:18:01 -0400

    bitacora done

--- app/Http/Controllers/bitatoalla/bitatoallaController.php
+++ app/Http/Controllers/bitatoalla/bitatoallaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\bitatoalla;
 
 use Illuminate\Http\Request;
 use App\Core\CrudController;
+use App\Models\bitatoalla;
 use App\Services\bitatoalla\bitatoallaService;
 /** @property bitatoallaService $service */
 class bitatoallaController extends CrudController
@@ -11,5 +12,12 @@ class bitatoallaController extends CrudController
     public function __construct(bitatoallaService $service)
     {
         parent::__construct($service);
+    }
+
+    public function bita(Request $request)
+    {
+        $r=$request->get('id');
+        $bus=bitatoalla::where('id_toalla',$r)->get();
+        return  ["list"=>$bus,'total'=>count($bus)];
     }
 }
