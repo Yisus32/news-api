5fa9d62893af0004d6f50b5c75494da50025b0c5
jesus <altuvejesus@zippyttech.com>
2021-11-12 13:44:17 -0400

    se agrego la hora del teetime al index del modulo de reservaciones

--- app/Repositories/Reservation/ReservationRepository.php
+++ app/Repositories/Reservation/ReservationRepository.php
@@ -62,6 +62,7 @@ class ReservationRepository extends CrudRepository
             $reservation['teetime_cancel_time']);
             $reservation['created_at'] = Carbon::parse($reservation['created_at'])->format('Y-m-d',env('APP_TIMEZONE'));
             $reservation['updated_at'] = Carbon::parse($reservation['created_at'])->format('Y-m-d',env('APP_TIMEZONE'));
+            $reservation['teetime_date_start'] = Carbon::parse($reservation['teetime_date_start'].' '.$reservation['teetime_hout_start'])->format('d-m-Y h:i A');
         }
 
         return $reservations;
