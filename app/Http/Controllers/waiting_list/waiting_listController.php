455768ee780d5ded67715971f895c212cd9eb6f8
henry <henryrom57@gmail.com>
2021-12-09 17:53:01 -0400

    ca

--- app/Http/Controllers/waiting_list/waiting_listController.php
+++ app/Http/Controllers/waiting_list/waiting_listController.php
@@ -44,6 +44,7 @@ class waiting_listController extends CrudController
         $tite="notificacion de reserva";
         $cuerpo="se cancelo una reservacion en la fecha:'.$date.' y hora:'.$hour.' la puedes tomar";
         $se= $client->_sendNotification(28,$tite,$cuerpo);
+        dd($se);
         foreach ($espera as $key) 
         {
             $id=$key->user_id;
@@ -51,7 +52,7 @@ class waiting_listController extends CrudController
           $se= $client->_sendNotification(28,$tite,$cuerpo);
         }
 
-        dd($se);
+       
 
     }
 }
